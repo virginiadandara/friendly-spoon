a4c07503be2bd5f2da0274763236bac333e89ce6
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-05-14 01:47:55 -0300

    Adicionando model Candlestick

--- candlesticks/models.py
+++ candlesticks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Candlestick(models.Model):
-	datetime = models.DateTimeField()
+	datetime = models.DateTimeField(unique=True, db_index=True)
 	open = models.FloatField()
 	high = models.FloatField()
 	low = models.FloatField()
@@ -10,3 +10,6 @@ class Candlestick(models.Model):
 	volume_btc = models.FloatField()
 	volume_currency = models.FloatField()
 	weighted_price = models.FloatField()
+
+	def __repr__(self):
+		return f'<Candlestick {self.timestamp}>'
