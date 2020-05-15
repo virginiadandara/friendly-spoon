705b9dbf34e9e1065c5a13cf6637231830ec0179
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-05-15 06:25:02 -0300

    Tornando métodos privados para facilitar entendimento da interface pública do código de populate.py

--- candlesticks/management/commands/populate.py
+++ candlesticks/management/commands/populate.py
@@ -14,40 +14,40 @@ class Command(BaseCommand):
 	$ python manage.py populate caminho/para/planila.csv
 	'''
 
-	def handle(self, *args, **options):
-		self.import_csv(options['csv_file'])
-		self.drop_nan_rows()
-		self.parse_column_names()
-		self.parse_timestamp()
-		self.batch_save()
-
-	def import_csv(self, path):
-		self.stdout.write('Lendo CSV...')
-		self.df = pd.read_csv(path)
-
 	def add_arguments(self, parser):
 		# Argumento único: CSV usado para popular o banco
 		parser.add_argument('csv_file', type=str)
 
+	def handle(self, *args, **options):
+		self._import_csv(options['csv_file'])
+		self._drop_nan_rows()
+		self._parse_column_names()
+		self._parse_timestamp()
+		self._batch_save()
+
+	def _import_csv(self, path):
+		self.stdout.write('Lendo CSV...')
+		self.df = pd.read_csv(path)
+
 	@atomic
-	def batch_save(self):
+	def _batch_save(self):
 		self.stdout.write('Criando objetos...')
-		batch = self.df.apply(self.build_model, axis=1).tolist()
+		batch = self.df.apply(self._build_model, axis=1).tolist()
 		self.stdout.write('Salvando no banco...')
 		Candlestick.objects.bulk_create(batch)
 		self.stdout.write(f'Sucesso!')
 
-	def build_model(self, row):
+	def _build_model(self, row):
 		cs = Candlestick()
 		for col, value in row.items():
 			setattr(cs, col, value)
 		return cs
 
-	def drop_nan_rows(self):
+	def _drop_nan_rows(self):
 		self.stdout.write('Eliminando linhas nulas...')
 		self.df.dropna(inplace=True)
 
-	def parse_column_names(self):
+	def _parse_column_names(self):
 		'''
 		Renomea as colunas para o padrão do banco (tudo minústula, 
 		apenas caracteres alfanuméricos e underline)
@@ -61,7 +61,7 @@ class Command(BaseCommand):
 			rename_dict[col] = new_name
 		self.df.rename(columns=rename_dict, inplace=True)
 
-	def parse_timestamp(self):
+	def _parse_timestamp(self):
 		self.stdout.write('Tratando timestamps...')
 		self.df['datetime'] = self.df['timestamp'].apply(datetime.fromtimestamp)
 		self.df.drop(columns=['timestamp'], inplace=True)
