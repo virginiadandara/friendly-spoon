a6ae4171d96fe002295d127c5b8f0a132f7c4dff
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-05-15 20:03:48 -0300

    Modificando o formato de QuerySet para obter os valores finais de cada dia, de forma reaproveitável para outros índices

--- candlesticks/models.py
+++ candlesticks/models.py
@@ -1,49 +1,13 @@
-from functools import cached_property
-from statistics import mean
-
 from django.db import models
 
 # Create your models here.
-class CandlestickQuerySet(models.QuerySet):
+class ClandlestickEndOfTheDayManager(models.Manager):
 	'''
-	QuerySet customizado capaz de calcular índices especificos para
-	um dado conjunto de candlesticks.
-	'''
-	
-	def media_movel_exponencial(self, period):
-		'''
-		Calcula a média móvel exponencial do queryset. Todos os candlesticks 
-		contidos no queryset são usados (e não apenas o do fim do dia).
-
-		Parâmetros
-		----------
-		period: int
-			O período / tipo da média exponencial (MME 20, 50, 100, 200, etc.)
-
-		Retorno
-		----------
-		out: list
-			Lista com as médias móveis exponenciais no período da queryset.
-		'''
-		out = []
-		k = 2 / (1 + period)
-		queryset = self.order_by('datetime')
-		values = list(map(lambda c: c.close, queryset))
-		mme = mean(values[:period])
-		out.append(mme)
-		for close in values[period:]:
-			mme += (close - mme) * k
-			out.append(mme)
-		return out
-
-class CandlestickManager(models.Manager):
-	'''
-	Model Manager que retorna uma Queryset do tipo CandlestickQuerySet,
-	capaz de calcular índices como a média móvel exponencial.
+	Manager que retorna a última instância de candlestick de cada dia
 	'''
 
 	def get_queryset(self):
-		return CandlestickQuerySet(self.model, using=self._db)
+		return super().get_queryset().order_by('-datetime__date', '-datetime').distinct('datetime__date')
 
 class Candlestick(models.Model):
 	datetime = models.DateTimeField(unique=True, db_index=True)
@@ -55,7 +19,39 @@ class Candlestick(models.Model):
 	volume_currency = models.FloatField()
 	weighted_price = models.FloatField()
 
-	objects = CandlestickManager()
+	objects = models.Manager()
+	end_of_the_day = ClandlestickEndOfTheDayManager()
 
 	def __repr__(self):
 		return f'<Candlestick {self.datetime}>'
+
+	@classmethod
+	def media_movel_exponencial(cls, period, queryset):
+		'''
+		Calcula a média móvel exponencial de um queryset dado. Todos os 
+		candlesticks contidos no queryset são usados (e não apenas o do fim do 
+		dia).
+
+		Parâmetros
+		----------
+		period: int
+			O período / tipo da média exponencial (MME 20, 50, 100, 200, etc.)
+		queryset: iterable
+			Objeto list-alike contento vários objetos do tipo Candlestick
+
+		Retorno
+		----------
+		out: dict
+			Dicionário com as médias móveis exponenciais no período da queryset.
+		'''
+
+		out = {}
+		k = 2 / (1 + period)
+		queryset = sorted(queryset, key=lambda cs: cs.datetime)
+		cs = queryset[0]
+		mme = cs.close
+		out[cs.datetime] = mme
+		for cs in queryset[1:]:
+			mme += (cs.close - mme) * k
+			out[cs.datetime] = mme
+		return out
--- candlesticks/tests.py
+++ candlesticks/tests.py
@@ -7,11 +7,15 @@ from .factories import CandlestickFactory
 
 # Create your tests here.
 class CandlestickTests(TestCase):
-	def test_media_movel_exponencial_50(self):
-		candlesticks = CandlestickFactory.create_batch(1000)
-		Candlestick.objects.bulk_create(candlesticks)
+	def test_media_movel_exponencial_20(self):
+		candlesticks = []
 		stop = datetime.now()
-		start = stop - timedelta(days=1)
-		qs = Candlestick.objects.filter(datetime__lte=stop, datetime__gte=start)
-		result = qs.media_movel_exponencial(50)
-		self.assertEqual(result[-1], 974.5)
+		start = stop - timedelta(days=100)
+		candlesticks.append(CandlestickFactory.build(datetime=stop))
+		for i in range(1, 100):
+			cs = CandlestickFactory.build(datetime=start + timedelta(days=i))
+			candlesticks.append(cs)
+		Candlestick.objects.bulk_create(candlesticks)
+		qs = Candlestick.end_of_the_day.filter(datetime__gte=start, datetime__lte=stop)
+		result = Candlestick.media_movel_exponencial(20, qs)
+		self.assertEqual(round(result[stop], 3), 80.977)
