bd6283e1a131437e20a1b8ae8fceda7c45d0c4ba
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-05-15 06:26:24 -0300

    Criado Manager e QuerySet customizados para o modelo Candlestick, capazdes de calcular a MME para qualquer dado queryset

--- candlesticks/models.py
+++ candlesticks/models.py
@@ -1,6 +1,30 @@
+from functools import cached_property
+from statistics import mean
+
 from django.db import models
 
 # Create your models here.
+class CandlestickQuerySet(models.QuerySet):
+	'''
+	QuerySet customizado capaz de calcular índices especificos para
+	um dado conjunto de candlesticks.
+	'''
+	def media_movel_exponencial(self, period):
+		out = []
+		k = 2 / (1 + period)
+		queryset = self.order_by('datetime')
+		values = list(map(lambda c: c.close, queryset))
+		mme = mean(values[:period])
+		out.append(mme)
+		for close in values[period:]:
+			mme += (close - mme) * k
+			out.append(mme)
+		return out
+
+class CandlestickManager(models.Manager):
+	def get_queryset(self):
+		return CandlestickQuerySet(self.model, using=self._db)
+
 class Candlestick(models.Model):
 	datetime = models.DateTimeField(unique=True, db_index=True)
 	open = models.FloatField()
@@ -10,6 +34,8 @@ class Candlestick(models.Model):
 	volume_btc = models.FloatField()
 	volume_currency = models.FloatField()
 	weighted_price = models.FloatField()
+
+	objects = CandlestickManager()
 
 	def __repr__(self):
 		return f'<Candlestick {self.datetime}>'
