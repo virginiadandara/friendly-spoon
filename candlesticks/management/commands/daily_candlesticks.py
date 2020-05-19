9e9f32360eb6166ac789a9b2eec8aa63e6b77745
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-05-19 19:31:15 -0300

    Comando para calcular os candlesticks diários adicionado

---
+++ candlesticks/management/commands/daily_candlesticks.py
@@ -0,0 +1,54 @@
+from functools import cached_property
+
+from django.core.management.base import BaseCommand
+from django.db.models import Max, Min, F
+from django.db.transaction import atomic
+
+from candlesticks.models import Candlestick
+
+
+class Command(BaseCommand):
+	'''
+	Comando para calcular abertura, fechamento, máx e mín diário.
+	Uso:
+	$ python manage.py daily_candlesticks
+	'''
+
+	@atomic
+	def handle(self, *args, **options):
+		candlesticks = []
+		for i in range(len(self.open)):
+			cs = Candlestick(type=Candlestick.DAY)
+			cs.open = self.open[i].open
+			cs.close = self.close[i].close
+			cs.high = self.high[i]['high__max']
+			cs.low = self.low[i]['low__min']
+			cs.datetime = self.open[i].datetime.date()
+			candlesticks.append(cs)
+		Candlestick.objects.bulk_create(candlesticks)
+
+	@cached_property
+	def open(self):
+		return Candlestick.objects.raw('''
+			SELECT DISTINCT ON (DATE("datetime")) id, open
+			FROM candlesticks_candlestick
+			WHERE type = 0
+			ORDER BY DATE("datetime"), "datetime" ASC;
+		''')
+
+	@cached_property
+	def close(self):
+		return Candlestick.objects.raw('''
+			SELECT DISTINCT ON (DATE("datetime")) id, close
+			FROM candlesticks_candlestick
+			WHERE type = 0
+			ORDER BY DATE("datetime"), "datetime" DESC;
+		''')
+
+	@cached_property
+	def high(self):
+		return Candlestick.objects.values('datetime__date').annotate(Max('high')).order_by('datetime__date')
+
+	@cached_property
+	def low(self):
+		return Candlestick.objects.values('datetime__date').annotate(Min('low')).order_by('datetime__date')
