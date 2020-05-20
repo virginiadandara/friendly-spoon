edbdbdaa367ba84dce9743b6c67a16c260efd111
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-05-20 01:17:39 -0300

    Modificando os testes da MME para coincidir com novo padrão onde period tem default = 20

--- candlesticks/tests.py
+++ candlesticks/tests.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from django.test import TestCase
 
 from .models import Candlestick
-from .services import MediaMovelExponencial
+from .services import MediaMovelExponencial, IndiceForcaRelativa
 from .factories import CandlestickFactory
 
 # Create your tests here.
@@ -21,5 +21,5 @@ class CandlestickTests(TestCase):
 
 	def test_media_movel_exponencial_20(self):
 		start, stop = self.create_candlesticks(100)
-		result = MediaMovelExponencial(20, start, stop + timedelta(days=1)).execute()
+		result = MediaMovelExponencial(start, stop + timedelta(days=1)).execute()
 		self.assertEqual(round(result[-1], 3), 80.976)
