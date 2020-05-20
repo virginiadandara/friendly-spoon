7e88f69a5d614182b4c174f9d18e5865b99f1b55
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-05-20 00:17:30 -0300

    Mudando settings para usar pathlib

--- smarttbot/settings.py
+++ smarttbot/settings.py
@@ -10,14 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+# import os
 from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = Path(__file__).resolve().parents[1]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
